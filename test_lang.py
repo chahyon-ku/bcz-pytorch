2d2081d86f98799e17d802a8fda4d4de4d17b4fa
SageRobots <carlewinge@gmail.com>
2023-04-23 19:34:12 -0500

    2D language conditioned model

---
+++ test_lang.py
@@ -0,0 +1,86 @@
+import time
+import rlbench.utils
+import rlbench.action_modes.action_mode
+import rlbench.action_modes.arm_action_modes
+import rlbench.action_modes.gripper_action_modes
+import rlbench.observation_config
+import rlbench.tasks.reach_target
+import numpy as np
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import torch
+import sentence_transformers
+from rlbench.const import colors
+import torchvision
+import tqdm
+import os
+import logging
+import wandb
+import matplotlib.pyplot as plt
+from hydra.core.hydra_config import HydraConfig
+from PIL import Image
+os.environ['TOKENIZERS_PARALLELISM']='true'
+import clip
+
+def test(task, model, language_encoder, n_episodes, n_steps_per_episode, device, environment):
+    model = model.to(device)
+    model.eval()
+    task = environment.get_task(hydra.utils.get_class(task))
+    success = 0
+    rgbs = []
+    for i_episode in range(n_episodes):
+        rgbs.append([])
+        if i_episode % 3 == 0:
+            task.set_variation(0)
+        elif i_episode % 3 == 1:
+            task.set_variation(1)
+        else:
+            task.set_variation(2)
+        descriptions, obs = task.reset()
+        description = descriptions[0]
+        # tokenize description
+        text = clip.tokenize([description]).to(device)
+        task_embed = language_encoder.encode_text(text).float()
+        for j in range(n_steps_per_episode):
+            rgbs[-1].append(obs.front_rgb.copy().transpose(2, 0, 1))
+            # image = Image.fromarray(obs.front_rgb)
+            # plt.imshow(image)
+            # plt.show()
+            action = model.get_action(obs, task_embed)
+            try:
+                obs, reward, terminate = task.step(action)
+            except rlbench.backend.exceptions.InvalidActionError:
+                print('Invalid action')
+                break
+            if terminate:
+                if reward == 1:
+                    print('Success')
+                    success += 1
+                # do 10 more steps then break
+                for k in range(10):
+                    rgbs[-1].append(obs.front_rgb.copy().transpose(2, 0, 1))
+                    action = model.get_action(obs, task_embed)
+                    try:
+                        obs, reward, terminate = task.step(action)
+                    except rlbench.backend.exceptions.InvalidActionError:
+                        print('Invalid action')
+                        break
+                break
+
+    # environment.shutdown()
+    return success / n_episodes, rgbs
+
+
+@hydra.main(version_base=None, config_path='./configs/test', config_name='reach_target')
+def main(cfg: DictConfig) -> None:
+    output_dir = HydraConfig.get().run.dir
+
+    logging.getLogger('sentence_transformers.SentenceTransformer').setLevel(logging.ERROR)
+    language_encoder = hydra.utils.instantiate(cfg.language_encoder)
+    model = hydra.utils.instantiate(cfg.model)
+
+    hydra.utils.instantiate(cfg.test, model=model, language_encoder=language_encoder)
+        
+
+if __name__ == '__main__':
+    main()
---
+++ train_lang.py
@@ -0,0 +1,130 @@
+import time
+from matplotlib import pyplot as plt
+import rlbench.utils
+import rlbench.action_modes.action_mode
+import rlbench.action_modes.arm_action_modes
+import rlbench.action_modes.gripper_action_modes
+import rlbench.observation_config
+import rlbench.tasks.reach_target
+import numpy as np
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import torch
+import sentence_transformers
+from rlbench.const import colors
+import torchvision
+import tqdm
+import os
+import logging
+import wandb
+from hydra.core.hydra_config import HydraConfig
+os.environ['TOKENIZERS_PARALLELISM']='true'
+import clip
+
+@hydra.main(version_base=None, config_path='./configs/train/single_task', config_name='reach_target_lang')
+def main(cfg: DictConfig) -> None:
+    output_dir = HydraConfig.get().run.dir
+    input(cfg)
+
+    # create clip model
+    device = 'cuda'
+    clip_model, preprocess = clip.load("ViT-B/32", device=device)
+    clip_model.eval()
+    # create list of text descriptions
+    task_embeds = {'reach_target': []}
+    text = []
+    text.append('reach the red target')
+    text.append('reach the green target')
+    text.append('reach the blue target')
+    # get embeddings
+    text = clip.tokenize(text).to(device)
+    with torch.no_grad():
+        text_features = clip_model.encode_text(text)
+    # put in dictionary
+    for i in range(3):
+        task_embeds['reach_target'].append(text_features[i].float())
+
+    environment = hydra.utils.instantiate(cfg.environment)
+    train_dataset = hydra.utils.instantiate(cfg.train_dataset, task_embeds=task_embeds)
+    train_dataloader = hydra.utils.instantiate(cfg.train_dataloader, dataset=train_dataset)
+    train_dataloader_iter = iter(train_dataloader)
+    model = hydra.utils.instantiate(cfg.model)
+    optimizer = hydra.utils.instantiate(cfg.optimizer, params=model.parameters())
+    # loss_fn = torch.nn.MSELoss()
+    loss_fn = torch.nn.HuberLoss()
+
+    # configure logging
+    wandb.login()
+    run = wandb.init(
+        dir=output_dir,
+        config=OmegaConf.to_container(cfg, resolve=True),
+        id=os.path.basename(output_dir),
+        **cfg.wandb
+    )
+
+    model.to(cfg.train.device)
+    step_tqdm = tqdm.tqdm(range(1, cfg.train.n_steps + 1))
+    train_losses = []
+    best_val_loss = float('inf')
+    for step in step_tqdm:
+        model.train()
+        if step == 6000:
+            for g in optimizer.param_groups:
+                g['lr'] = 1e-4
+        if step == 9000:
+            for g in optimizer.param_groups:
+                g['lr'] = 5e-5
+        try:
+            image, task_embed, xyz, axangle, gripper = next(train_dataloader_iter)
+        except StopIteration:
+            print('restarting train dataloader')
+            train_dataloader_iter = iter(train_dataloader)
+            image, task_embed, xyz, axangle, gripper = next(train_dataloader_iter)
+        model.train()
+        image = image.to(cfg.train.device)
+        action = xyz.to(cfg.train.device)
+        # if step <= 10:
+        #     print(np.linalg.norm(xyz, axis=-1), np.linalg.norm(axangle, axis=-1), gripper)
+        #     plt.imshow(image[0].permute(1, 2, 0).cpu().numpy())
+        #     plt.title('xyz' + str(action[0, 0, :].cpu().numpy()))
+        #     plt.show()
+        task_embed = task_embed.to(cfg.train.device)
+        optimizer.zero_grad()
+        pred_action = model.forward(image, task_embed)
+        # input(pred_action[1].shape)
+        # input(action[1].shape)
+        # scale action y z by 10
+        action_scale = 20
+        loss = loss_fn(pred_action, action_scale*action[:, 0, 1:])*100
+        # if step % 100 == 0:
+        #     print('pred_action', pred_action[0].detach().cpu().numpy())
+        #     print('action', action_scale*action[0, 0, :].detach().cpu().numpy())
+            
+        loss.backward()
+        optimizer.step()
+
+        # for k, v in loss.items():
+        #     train_losses.setdefault(k, []).append(v.item())
+        
+        # if step % cfg.train.log_interval == 0:
+        #     for k, v in train_losses.items():
+        #         wandb.log({'train/loss/%s' % k: np.mean(v)}, step=step)
+        #     train_losses = {}
+
+        train_losses.append(loss.item())
+        if step % cfg.train.log_interval == 0:
+            wandb.log({'train/loss/':np.mean(train_losses)}, step=step)
+            train_losses = []
+
+        if step % cfg.train.eval_interval == 0:
+            success_rate, rgbs = hydra.utils.instantiate(cfg.test, model=model, language_encoder=clip_model, environment=environment)
+            wandb.log({'test/success_rate': success_rate}, step=step)
+            wandb.log({'test/rgb': [wandb.Video(np.stack(rgb, axis=0), fps=10) for rgb in rgbs]}, step=step)
+            
+        
+        if step % cfg.train.save_interval == 0:
+            torch.save(model.state_dict(), os.path.join(output_dir, f'model_{step}.pt'))
+        
+
+if __name__ == '__main__':
+    main()
